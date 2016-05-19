ace1f98875eee35577a9f2f357d320fa404d0334
Stephen Braswell <stephen@unc.edu>
2016-05-19 19:47:38 -0400

    stuff

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 
-phpinfo();
+$dbserver = '172.30.105.206';
+$dbuser   = 'userL2R';
+$dbpass   = 'v78OIQro1hF0TJrv';
+$dbport   = '3306';
+$dbname   = 'sampledb';
+
+$conn = new mysqli($dbserver, $dbuser, $dbpass, $dbname, $dbport);
+
+if ($conn->connect_errno) 
+{
+   die("Connection failed: " . $conn->connect_errno);
+}
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+while($row = $result->fetch_assoc()) 
+{
+echo $row['name'];
+}
